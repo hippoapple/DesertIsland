a30558c8cd558f8d6fdb3f200c0323552f019451
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-07-06 20:18:57 +0900

    수경 빠이어 스크립트 추가

---
+++ Assets/02. Scripts/SK/Fire.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Fire : MonoBehaviour
+{
+    public GameObject gauge;
+    public float progress = 0f;
+
+    bool isTriggerEnter = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTriggerEnter == true)
+        {
+            progress += Time.deltaTime * 5;
+            gauge.gameObject.GetComponentInChildren<Slider>().value = progress;
+        }
+
+        if (progress == 10)
+        {
+
+        }
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Branch")
+        {
+            isTriggerEnter = true;
+            gauge.gameObject.SetActive(true);
+        }
+           
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.gameObject.tag == "Branch")
+        {
+            isTriggerEnter = true;
+            print("Enter");
+            gauge.gameObject.SetActive(true);
+
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        isTriggerEnter = false;
+        gauge.gameObject.SetActive(false);
+        progress = 0f;
+        GetComponentInChildren<Slider>().value = 0f;
+
+
+    }
+
+}
