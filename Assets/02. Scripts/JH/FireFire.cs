86782df9b1fde939e508da5c371a49518f770986
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-06 21:39:14 +0900

    firefire

---
+++ Assets/02. Scripts/JH/FireFire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFire : MonoBehaviour
+{
+    int count = 0 ;
+    public GameObject firePlace;
+    public GameObject rocks;
+    public GameObject wood;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firePlace.gameObject.SetActive(false );
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        
+        if(other.gameObject.tag =="WOOD")
+        {
+            count+=1;
+            Debug.Log($"count = {count}");
+        }
+        
+        if(count==3)
+        {
+            firePlace.gameObject.SetActive(true );
+            rocks.gameObject.SetActive(false );
+            wood.gameObject.SetActive(false );
+
+        }
+    }
+}
