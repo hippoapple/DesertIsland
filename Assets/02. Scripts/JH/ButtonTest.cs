bbc77a81723e844525040a7014c4197cba9ab675
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-07 18:07:44 +0900

    asap

---
+++ Assets/02. Scripts/JH/ButtonTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ButtonTest : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        Debug.Log("BUTTON CLICKED");
+        SceneManager.LoadScene("ViveSetting_JH");
+    }
+}
