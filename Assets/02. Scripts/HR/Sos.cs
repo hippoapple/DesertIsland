ffb81404bd57ef5fb0d5ded9f781335890673811
haeri-park <haeri0915@naver.com>
2020-07-09 18:06:33 +0900

    Flag, Sos 스크립트 작성

--- Assets/02. Scripts/HR/Sos.cs
+++ Assets/02. Scripts/HR/Sos.cs
@@ -5,28 +5,19 @@ using UnityEngine;
 public class Sos : MonoBehaviour
 {
     public float speed = 1;
-    Vector3 dir;
 
-    // 해골과 돌들이 Y축으로 올라온다.
-    // 태그가 SOS면
-    // 만약 깃발을 잡으면
+    void update() 
+    {
+        transform.position += new Vector3(0.0f, 1.2f, 0.0f) * speed * Time.deltaTime;
+    }
+}
+    
+    // 해골과 돌들이 Y축 1.2 더올라온다.
+
+
+    
+
+    // --------------------------
 
     // 만약 존 안에 10개의 돌이 올라온다면
     // 헬기가 소리가 들리며 헬기가 온다.
-
-    void Start()
-    {
-
-
-    }
-
-
-    void update()
-    { 
-        //if(collision.gameObject.tag == "Flag")
-        //{
-        //transform.position + transform.position(0, 0.7, 0) * speed * Time.deltaTime;
-        //SOS.gameObject.SetActive(true);
-        //}
-    }
-}
