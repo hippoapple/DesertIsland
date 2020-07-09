78638f6157bcdbee20c10af7deb935a4c88d5234
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-07-09 18:07:14 +0900

    메인 텍스트 기능 추가중 젠장

---
+++ Assets/02. Scripts/SK/TextManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR;
+public class TextManager : MonoBehaviour
+{
+   private SteamVR_Action_Boolean tirgger = SteamVR_Actions.default_InteractUI;
+      public Text [] hintmasage = new Text [4] ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
