aeed3437c67f6947385ba0d4c921b79eb465c16a
haeri-park <haeri0915@naver.com>
2020-07-10 16:50:52 +0900

    ???

--- Assets/02. Scripts/HR/StoneZone.cs
+++ Assets/02. Scripts/HR/StoneZone.cs
@@ -4,38 +4,69 @@ using UnityEngine;
 
 public class StoneZone : MonoBehaviour
 {
+    /*public int i = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int i = 0; i < 10; i++)
+        {
+            if(i == 9)
+            {
+                break;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //만약 StoneZone 안에 tag가 stone인게 10개 들어 온다면
-        // sos2가 setactive 되며
-        // 기존sos와, 돌멩이들이 삭제 된다.
-        
+    
     }
-        int count = 0;
 
+    public GameObject sOS2;
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Stone"))
         {
-            count++;
-            Debug.Log("count =" + count);
+            sOS2.gameObject.SetActive(true);
+            sOS.gameObject.SetActive(false);
+            .gameObject.SetActive(false); 
         }
     }
-    private void OnCollisionExit(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Stone"))
-        {
-            count--;
-            Debug.Log("count =" + count);
-        }
-    }
-    
+    */
 
+        // tag가 Stone인게 10개 들어 온다면
+        // 근데 만약 StoneZone 안에 
+        // sos2가 setactive 되며
+        // StoneZone 안의 tage가 Stone 인 돌멩이들이 삭제 된다.
+    
+    public static int count = 0 ;
+    public GameObject stoneZone;
+    public GameObject sOS2;
+
+    public List<GameObject> storage = new List<GameObject>(); 
+
+    private int stoneCount = 0;
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.CompareTag("Stone"))
+        {
+            if (++stoneCount == 9)
+            {
+                sOS2.gameObject.SetActive(true);
+                DestroyStones();
+            }
+            // Debug.Log($"count = {count}");
+        }
+    }
+    void DestroyStones()
+    {
+        Collider[] colls = Physics.OverlapSphere(transform.position, 10.0f);
+        foreach(Collider coll in colls)
+        {
+            Destroy(coll.gameObject);
+        }
+    }
 }
+
