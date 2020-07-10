5e20d0e2a90e1be89af2c1e4f5a9a9b797750fd3
haeri-park <haeri0915@naver.com>
2020-07-10 10:52:29 +0900

    재업로드

--- Assets/02. Scripts/GrabMgr.cs
+++ Assets/02. Scripts/GrabMgr.cs
@@ -11,20 +11,19 @@ public class GrabMgr : MonoBehaviour
     //공에 접촉했는지 여부
     private bool isTouched = false;
 
-    private SteamVR_Input_Sources hand = SteamVR_Input_Sources.Any; //양손을 사용할 수 있도록 하는 변수
-    private SteamVR_Action_Boolean tirgger ;
+  
+    SteamVR_Input_Sources hand;
+    SteamVR_Action_Boolean tirgger;
+    public GameObject SOS;
+    public float speed = 1;
 
     void Awake()
     {
+        hand = SteamVR_Input_Sources.Any; //양손을 사용할 수 있도록 하는 변수
         tirgger = SteamVR_Actions.default_InteractUI;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
+ 
     // Update is called once per frame
     void Update()
     {
@@ -59,11 +58,15 @@ public class GrabMgr : MonoBehaviour
         }
         grabObject = coll.transform;  
     
-	    if (coll.gameObject.CompareTag ("Flag"))
-        {
-           GameObject.Find("Flag").GetComponent<Flag>().SosMake();
-        }
-      
-    
+	    
     }
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.gameObject.CompareTag ("Flag"))
+        {
+           SOS.transform.position += new Vector3(0.0f, 1.2f, 0.0f) * speed * Time.deltaTime;
+        }
+    }
+
 }
