db718378168876a4420c12b871da4c8c36108051
haeri-park <haeri0915@naver.com>
2020-07-11 17:37:03 +0900

    헬기 target 방향 구현
    ㅠㅠ감동

---
+++ Assets/02. Scripts/HR/FlyCam.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyCam : MonoBehaviour
+{
+    public GameObject hc;
+    public Transform centerTr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Hashtable ht = new Hashtable();
+        ht.Add("time", 35.0f);
+        ht.Add("path", iTweenPath.GetPath("Fly"));
+        ht.Add("easetype", iTween.EaseType.easeInOutBack); //easeOutBounce통통튐
+        ht.Add("looktarget",centerTr.position);
+        ht.Add("looktime",0.2f);
+        // ht.Add("orienttopath", true); //길따라 보는것
+
+        iTween.MoveTo(this.gameObject, ht);
+    }
+}
