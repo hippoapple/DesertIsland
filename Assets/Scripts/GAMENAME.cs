8a1022e687cd7c2bbe472ad43e269a0722b5bb65
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-14 13:12:28 +0900

    bubble pop

---
+++ Assets/Scripts/GAMENAME.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
+
