ba5c6b87040bff7c6d56ee2b4646fa39f0ec7f94
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-07-14 12:11:53 +0900

    스크립트 수정3중

--- Assets/02. Scripts/SK/MesegesManager.cs
+++ Assets/02. Scripts/SK/MesegesManager.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MesegesManager : MonoBehaviour
-{   
-    public static MesegesManager instance;
+{  
     
      public GameObject Hint1_Canvas;
      public GameObject Hint2_Canvas;
@@ -16,10 +15,8 @@ public class MesegesManager : MonoBehaviour
      public GameObject Hint7_Canvas;
      public Slider FireSlider;
     public GameObject FirePlace;
-   private void Awake()
-   {
-       instance = this;
-   }
+
+    bool isCavasOn = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,40 +29,43 @@ public class MesegesManager : MonoBehaviour
        // StoryManager.timer += Time.deltaTime;
          //print( StoryManager.timer);
 
-        if( StoryManager.timer > 60 && FireFire.woodCount == 0 )
+        if( StoryManager.timer > 10 && FireFire.woodCount == 0 )
         {
+            Debug.Log("실행");
            Hint1_Canvas.SetActive(true); 
-           Invoke("CanvasOff",5);
+           isCavasOn = true;
+           
+           if( isCavasOn == true)
+           {
+                Invoke("CanvasOff",5);
+                print("꺼져랏");
+           }
         }
 
-         if( StoryManager.timer >= 60 && FireSlider.value == 0 && FireFire.woodCount == 5);
+         if( StoryManager.timer > 10 && FireFire.woodCount == 5);
         {
          
             Hint2_Canvas.SetActive(true); 
             Invoke("CanvasOff",5);
             // StoryManager.timer = 0;
         }
-        if( StoryManager.timer >= 60 && FireSlider.value == 0 && FireFire.woodCount ==3);
-        {
-         
-            Hint3_Canvas.SetActive(true); 
-            Invoke("CanvasOff",5);
-            // StoryManager.timer = 0;
-        }
+        
+
     }
 
-    void CanvasOn()
-    {
-      
-    }
 
     void CanvasOff()
     {
         Hint1_Canvas.SetActive(false); 
+        print("1꺼짐");
         Hint2_Canvas.SetActive(false); 
+        print("2꺼짐");
         Hint3_Canvas.SetActive(false); 
+        print("3꺼짐");
         Hint4_Canvas.SetActive(false); 
+        print("4꺼짐");
         Hint5_Canvas.SetActive(false); 
+        print("5꺼짐");
         Hint6_Canvas.SetActive(false); 
     }
 }
