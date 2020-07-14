0867d41522d89b7e14c182c165d3c7f642b937a4
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-14 14:49:15 +0900

    가즈아!

--- Assets/02. Scripts/SK/MesegesManager.cs
+++ Assets/02. Scripts/SK/MesegesManager.cs
@@ -49,28 +49,26 @@ public class MesegesManager : MonoBehaviour
             isCavasOn = true;
             Invoke("CanvasOff", 5);
         }
-
-
     }
 
 
-    void CanvasOff()
-    {
-        Debug.Log("이것도 실행 될까?");
-        print("에잉");
-        //isCavasOn = false;
-        Hint1_Canvas.SetActive(false);
-        print("1꺼짐");
-        Hint2_Canvas.SetActive(false);
-        //print("2꺼짐");
-        Hint3_Canvas.SetActive(false);
-        //print("3꺼짐");
-        Hint4_Canvas.SetActive(false);
-        //print("4꺼짐");
-        Hint5_Canvas.SetActive(false);
-        //print("5꺼짐");
-        Hint6_Canvas.SetActive(false);
+    // void CanvasOff()
+    // {
+    //     Debug.Log("이것도 실행 될까?");
+    //     print("에잉");
+    //     //isCavasOn = false;
+    //     Hint1_Canvas.SetActive(false);
+    //     print("1꺼짐");
+    //     Hint2_Canvas.SetActive(false);
+    //     //print("2꺼짐");
+    //     Hint3_Canvas.SetActive(false);
+    //     //print("3꺼짐");
+    //     Hint4_Canvas.SetActive(false);
+    //     //print("4꺼짐");
+    //     Hint5_Canvas.SetActive(false);
+    //     //print("5꺼짐");
+    //     Hint6_Canvas.SetActive(false);
         
-        storyManager.StopAllCoroutines();
-    }
+    //     storyManager.StopAllCoroutines();
+    // }
 }
