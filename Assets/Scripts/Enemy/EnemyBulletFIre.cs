d137b704d2689031f1f18f147ff86a49e1973dc7
hippoapple <39970069+hippoapple@users.noreply.github.com>
2020-07-14 13:19:28 +0900

    20200717jk

---
+++ Assets/Scripts/Enemy/EnemyBulletFIre.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletFIre : MonoBehaviour
+{
+    //나는 총을 한방만 쏠꺼야!!
+    //필요 에너미의 y값 총알 공장 일정 시간 경과시간
+    public GameObject bulletFactory;
+    public GameObject enemy;
+    public float createTime = 1;
+    float currentTime;
+    bool a = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //한번만 발사했니
+        //만약에 너가 한번 발사를 했다면 나는 조건을 true로 바꿀꺼야
+        if (a == false)
+        {
+            currentTime += Time.deltaTime;
+            if (currentTime > createTime)
+            {
+                GameObject bullet = Instantiate(bulletFactory);
+                bullet.transform.position = enemy.transform.position;
+                currentTime = 0;
+                a = true;
+            }            
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    //나는 상하 좌우로 움직일 것이다.
+    //필요 속성 방향 속도
+    public float speed = 5;
+    Vector3 dir;
+
+    public float minX ;
+    public float maxX ;
+    public float minY ;
+    public float maxY ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //상하좌우로 입력받을 것이다.
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        //방향을 정할 것이야
+        dir = new Vector3(h, v, 0);
+        dir.Normalize();
+        //이동할 것이야
+        transform.position += dir * speed * Time.deltaTime;
+
+        //이동제한할 수 있도록 할것이야
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3.2f, 3.2f), Mathf.Clamp(transform.position.y, -4.0f, 6.0f), 0);
+    }
+        //아래에 피 통 닳는 것 넣을 것이야
+}
