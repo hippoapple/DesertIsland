dbffca379b3cce7c0eaa810d0fd1ee80753b4491
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-14 18:58:07 +0900

    potposition

---
+++ Assets/02. Scripts/JH/PotPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotPosition : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.transform.position= new Vector3(15.26f, 10.3f, -7.74f);
+    }
+}
