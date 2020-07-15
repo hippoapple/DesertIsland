8dc0787b74eee9aaccec5e5183e5e45b20696349
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-07-15 12:16:33 +0900

    스토리, 불 변수

--- Assets/02. Scripts/JH/BoilWater.cs
+++ Assets/02. Scripts/JH/BoilWater.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class BoilWater : MonoBehaviour
 {
     public GameObject gauge;
-    boolen istriggerenter=false;
+    //boolen istriggerenter =false;
     float progress;
     
     // Start is called before the first frame update
