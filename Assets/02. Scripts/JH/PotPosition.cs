a09a46d3a65f6834db86a342b155c2f79de02fc8
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-15 17:47:20 +0900

    potposition script change

--- Assets/02. Scripts/JH/PotPosition.cs
+++ Assets/02. Scripts/JH/PotPosition.cs
@@ -11,7 +11,10 @@ public class PotPosition : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.position= new Vector3(15.26f, 10.3f, -7.74f);
-        boilWater.BoiledWaterInPot();
+        if(other.gameObject.CompareTag("BigPot"))
+        {
+            other.transform.position= new Vector3(15.26f, 10.3f, -7.74f);
+            boilWater.BoiledWaterInPot();
+        }
     }
 }
