9668343f01532283c503fd6080e5c903381fe692
haeri-park <haeri0915@naver.com>
2020-07-15 16:45:25 +0900

    ???

---
+++ Assets/02. Scripts/HR/Final.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Final : MonoBehaviour
+{
+    public void ButtonAcitveOn()
+    {
+        gameObject.SetActive(true);
+    }
+    public void ButtonAcitveOff()
+    {
+        gameObject.SetActive(false);
+    }
+}
+
