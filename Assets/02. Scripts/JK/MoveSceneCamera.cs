91cc4aba08da735efa07755271c6d32c73ea661e
hippoapple <39970069+hippoapple@users.noreply.github.com>
2020-07-16 20:56:14 +0900

    20200716JK03

---
+++ Assets/02. Scripts/JK/MoveSceneCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveSceneCamera : MonoBehaviour
+{
+    float timer;
+    int waitingTime;
+    bool inside;
+    // Start is called before the first frame update
+    void Start()
+    {
+          timer = 0.0f;
+          waitingTime = 20;
+          inside = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          timer += Time.deltaTime;
+   
+         if(timer > waitingTime)
+        {
+            //Action
+            SceneManager.LoadScene("ViveSetting_JK_2");
+            timer = 0;
+        }
+
+    }
+}
