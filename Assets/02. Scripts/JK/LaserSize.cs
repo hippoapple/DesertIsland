d2b505d94445b4c308aefd28849c69196d9f39ec
haeri-park <haeri0915@naver.com>
2020-07-21 20:50:40 +0900

    준기님 에러 죽임

--- Assets/02. Scripts/JK/LaserSize.cs
+++ Assets/02. Scripts/JK/LaserSize.cs
@@ -10,22 +10,22 @@ public class LaserSize : MonoBehaviour
 
 
     // Start is called before the first frame update
-        Vector3 des = new Vector3(0, 0, maxDistance);
-        Vector3 origin = Vector3.zero;
     void Start()
     {
+        Vector3 des = new Vector3(0, 0, maxDistance);
+        Vector3 origin = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = (des - origin).magnitude;
-        if (distance < 1) {
+        //float distance = (des - origin).magnitude;
+        //if (distance < 1) {
                            
-            transform.localScale += new Vector3(0.1f,0.1f,0.1f);
+            //transform.localScale += new Vector3(0.1f,0.1f,0.1f);
             //transform.localScale.x += 0.1f;
             //transform.localScale.y += 0.1f;
             //transform.localScale.z += 0.1f;
-        }   
+        //}   
     }
 }
