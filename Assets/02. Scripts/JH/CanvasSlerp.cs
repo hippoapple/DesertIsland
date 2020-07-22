7d4f3a4a58de196bebb2f5882a6728d27f590c82
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-22 20:23:24 +0900

    modify

--- Assets/02. Scripts/JH/CanvasSlerp.cs
+++ Assets/02. Scripts/JH/CanvasSlerp.cs
@@ -5,26 +5,22 @@ using UnityEngine;
 public class CanvasSlerp : MonoBehaviour
 {
     Vector3 alpha;
+    Transform gamma;
     public GameObject playercanvas;
     Vector3 beta;
     // Start is called before the first frame update
     void Start()
     {
         alpha = GetComponent<Transform>().position;
+        gamma = GetComponent<Transform>();
         beta =playercanvas.transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        alpha= Vector3.Slerp(alpha, beta, 0.01f);
-        // if(beta.y>7.5f)
-        // {
-        //     alpha.y = 7.5f;
-        // }
-        // if(beta.y<4.5f)
-        // {
-        //     alpha.y =4.5f;
-        // }
+        //alpha = new Vector3(0, Mathf.Clamp(Time.time, 4.5f, 7.5f),0);
+        gamma.position= Vector3.Slerp(gamma.position, playercanvas.transform.position, 0.01f);
+        
     }
 }
