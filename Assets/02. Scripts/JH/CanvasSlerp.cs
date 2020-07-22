1e50d874eca3b0edebbce383bd28301625b5b75e
JaeHoon-J <67031899+JaeHoon-J@users.noreply.github.com>
2020-07-22 19:50:26 +0900

    modify

--- Assets/02. Scripts/JH/CanvasSlerp.cs
+++ Assets/02. Scripts/JH/CanvasSlerp.cs
@@ -6,15 +6,21 @@ public class CanvasSlerp : MonoBehaviour
 {
     Transform alpha;
     public GameObject playercanvas;
+    Vector3 beta;
     // Start is called before the first frame update
     void Start()
     {
         alpha = GetComponent<Transform>();
+        beta =playercanvas.transform.position; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        alpha.position = Vector3.Slerp(alpha.position, playercanvas.transform.position, 0.1f);
+        alpha.position = Vector3.Slerp(alpha.position, playercanvas.transform.position, 0.01f);
+        if(playercanvas.transform.position.y>7.5f)
+        {
+            //beta.y =
+        }
     }
 }
