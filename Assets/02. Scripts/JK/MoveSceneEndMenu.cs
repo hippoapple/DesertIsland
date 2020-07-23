f882bcb3c1b14a738fdff23dc8f9fdb42078fc76
HNCSoo <58102022+HNCSoo@users.noreply.github.com>
2020-07-23 12:57:03 +0900

    엔딩 메뉴 스테틱변수 초기화 완료

--- Assets/02. Scripts/JK/MoveSceneEndMenu.cs
+++ Assets/02. Scripts/JK/MoveSceneEndMenu.cs
@@ -5,19 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class MoveSceneEndMenu : MonoBehaviour
 {
-//    private void Update()
-//    {
-//         if (Input.GetMouseButtonDown(0))
-//         {
-//             SceneManager.LoadScene("ViveSetting_JK_2");
-//         }
-       
-//    }
+    //    private void Update()
+    //    {
+    //         if (Input.GetMouseButtonDown(0))
+    //         {
+    //             SceneManager.LoadScene("ViveSetting_JK_2");
+    //         }
 
-   public void GoToScene(string sceneName)
-   {
+    //    }
+
+    public void GoToScene(string sceneName)
+    {
         SceneManager.LoadScene(sceneName);
         StoryManager1.clickCount = -1;
-   }
+        FireFire.woodCount = 0;
+
+        PotPosition.isBigpotEnter = false;
+        PotPosition.isbigwateron = false;
+
+        ReadyWaterStep1.isWaterOn = false;
+        ReadyWaterStep1.IsSmallCup = false;
+        ReadyWaterStep1.IsPotLid = false;
+
+        StartFire.isFireOn = false;
+        DrinkWater.isDrink = false;
+        StoryManager1.clickCount = -1;
+        GrabMgr.isFlag = false;
+    }
 }
-   
+
